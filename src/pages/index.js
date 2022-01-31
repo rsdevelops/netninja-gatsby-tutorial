9124292a3d718d3c57c550e6ba3f0ec45d979b83
rsdevelops <rs9947@gmail.com>
2022-01-31 17:13:25 -0500

    minor home page change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,22 @@
 import React from "react"
-import { Link, graphql } from 'gatsby'
+import { Link, graphql } from "gatsby"
 import Layout from "../components/Layout"
-import styles from '../styles/home.module.css'
+import styles from "../styles/home.module.css"
 import Img from "gatsby-image"
 
 export default function Home({ data }) {
   console.log(data)
-  
+
   return (
     <Layout>
       <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop & Deploy</h3>
-          <p>UX designer & web developer based in Manchester.</p>
-          <Link className={styles.btn} to="/projects">My Portfolio Projects</Link>
+          <p>UX designer & web developer based in Charleston, SC.</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
         <Img fluid={data.file.childImageSharp.fluid} />
       </section>
@@ -24,7 +26,7 @@ export default function Home({ data }) {
 
 export const query = graphql`
   query Banner {
-    file(relativePath: {eq: "banner.png"}) {
+    file(relativePath: { eq: "banner.png" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid
